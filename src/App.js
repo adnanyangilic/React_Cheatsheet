572ae17cf0c623b68748c70d0aa149c4de757dc7
adnanyangilic <adnan.alistika@gmail.com>
2023-06-19 15:23:09 +0300

    Proje dosyası ekle.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const element = <h1>Hello, world!</h1>;
+
+  return (
+    <div>
+      {element}
+
+    </div>
+  );
+}
+
+export default App;
